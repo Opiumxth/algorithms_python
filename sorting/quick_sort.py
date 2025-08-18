5a0958108bc2cabc76b06c7fb6384ae3a3fc7d3c
Opiumxth <aki.chainsaw24@gmail.com>
2025-08-17 23:56:26 -0500

    First commit, setting structure for algorithms

---
+++ sorting/quick_sort.py
@@ -0,0 +1,4 @@
+
+# La idea con todos estos algoritmos es hacer un analisis de complejidad
+# Se planea que el documento ipynb se haga en jupyter notebook y ademas de
+# se redacte enteramente en ingles
