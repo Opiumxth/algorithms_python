f203d255a73969bc267df5db534a3f2823f2cab9
Opiumxth <aki.chainsaw24@gmail.com>
2025-09-30 08:20:18 -0500

    Set up initial file structure

---
+++ docs/main.typ
@@ -0,0 +1,2 @@
+= Subtitulo
+aaaaaa
--- sorting/quick_sort.py
+++
@@ -1,4 +0,0 @@
-
-# La idea con todos estos algoritmos es hacer un analisis de complejidad
-# Se planea que el documento ipynb se haga en jupyter notebook y ademas de
-# se redacte enteramente en ingles
